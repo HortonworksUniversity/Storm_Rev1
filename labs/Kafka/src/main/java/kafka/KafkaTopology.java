b4a311aeb9f69afdd160ccc6b8bbf376bfd75f88
rfraposa <rich@hortonworks.com>
2015-05-28 08:46:39 -0600

    Various lab file fixes

---
+++ labs/Kafka/src/main/java/kafka/KafkaTopology.java
@@ -0,0 +1,18 @@
+package kafka;
+
+import java.util.UUID;
+
+import storm.kafka.KafkaSpout;
+import storm.kafka.SpoutConfig;
+import storm.kafka.ZkHosts;
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.topology.TopologyBuilder;
+
+public class KafkaTopology {
+
+	public static void main(String[] args) {
+
+	}
+
+}
